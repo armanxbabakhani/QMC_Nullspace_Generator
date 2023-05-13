90573904413ae5458d6f2daa970163b2ee903dd6
Arman Babakhani <armanxbabakhani@berkeley.edu>
2023-05-12 23:41:40 -0700

    The PZComp is tested and sorted!

--- Null_Generator.cpp
+++ Null_Generator.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
-//#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
@@ -59,73 +59,125 @@ vector<pair<complex<double>, vector<int>>> data_extract(const string& fileName){
         inputFile.close();
     return data;
 }
-/*
+
 using boost::multiprecision::cpp_int;
 // ********* Computing the Zs and Ps and coefficients ******** //
 typedef vector<complex<double>> Coeffs;
-typedef vector<int> ZsVec;
+typedef vector<vector<int>> ZVecs;
 struct PZdata {
     vector<cpp_int> Ps;
     Coeffs coeffs;
-    ZsVec<int> Zs;
-    ZsVec<int> Z_track;
+    ZVecs Zs;
+    ZVecs Z_track;
 };
 
 PZdata PZcomp(const vector<pair<complex<double>,vector<int>>>& data) {
-    int l = data.size();
-    int z_count = 1, no_qubit = 0;
-    vector<complex<double>> coefficients(l);
+    PZdata PZ_data;
+    int l = data.size(),z_count = 0, no_qubit = 0;
     vector<cpp_int> Ps;
     Coeffs coeffs;
-    Indexed_Array<int> Zs(l); // This size is a max size. Some of Z[i]s might not be used!
+    ZVecs Zs;
+    ZVecs Z_track; //This vector maps the Zs to Ps it is a many to one mapping!
+
     for (int i = 0; i<l; i++){
-        coefficients[i] = data[i].first;
-        vector<int> data_i = data[i].second;
-        int num = 0;
-        ZsVec zs;
+        complex<double> coeff_i = data[i].first;
+        vector<int> zs_i; // For every line zs extracts the qubits on which a pauli Z acts! 
+        vector<int> data_i = data[i].second; // Extracts the array of qubits and paulis for every line of input!
+        int num = 0; // This variable keeps track of the index of the permutation matrix we get for each line of data!
+
         for (size_t j = 0; j < data_i.size() / 2; j++) {
-            int pauli_j = data_i[2 * j];
-            int qubit = data_i[2 * j - 1];
+            // Format of the input file: The 1st, 3rd, 5th, ... indicate the qubits
+            int qubit = data_i[2 * j];
+            // Format of the input file: The 2nd, 4th, 6th, ... indicate the paulis
+            int pauli_j = data_i[2 * j + 1];
+
             if (qubit > no_qubit)
                 no_qubit = qubit;
             if (pauli_j == 1) {
                 num += 1 << (qubit - 1);
             } else if (pauli_j == 2) {
                 num += 1 << (qubit - 1);
-                coefficients[i] *= complex<double>(0, 1);
-                zs.push_back(qubit);
+                coeff_i *= complex<double>(0, 1);
+                zs_i.push_back(qubit);
             } else if (pauli_j == 3) {
-                zs.push_back(qubit);
+                zs_i.push_back(qubit);
             }
         }
-        auto it = find(Ps.begin(), Ps.end(), num);
-
+        coeffs.push_back(coeff_i);
+        Zs.push_back(zs_i); // If zs is empty then no non-trivial diagonal components! (All identity operators)
+        
+        auto it = find(Ps.begin(), Ps.end(), num); // Look for num in the previous list of Ps (permutations)
+        
         if(it == Ps.end()) {
+            // num was not found in Ps, thus a new permutation matrix!
             Ps.push_back(num);
-            coeffs.push_back(coefficients[i]);
-            // Zs.push_back(zs);
-            Zs.add(z_count , zs);
-            z_count++;
+            // We will add i-th element of coeffs and Zs to be associated with the current Ps!
+            Z_track.push_back(vector<int> {i});
         } else {
-            int common_p = distance(Ps.begin(), it);
-            int lhz = Zs.get(common_p).size();
-            int common_l = 0;
-            for (int l = 0; l < lhz; l++) {
-                if (Zs.get(common_p)[l] == zs) {
-                    common_l = l;
+            // num was found in Ps, and P_index will be the index of Ps that matched num.
+            int P_index = std::distance(Ps.begin(), it);
+
+            vector<int> z_indices = Z_track[P_index];
+
+            bool z_found = false;
+            for (int k = 0; k < z_indices.size(); k++){
+                if (Zs[z_indices[k]] == zs_i){
+                    coeffs[P_index] += coeff_i;
+                    z_found = true;
                     break;
                 }
             }
-            if (common_l == 0) {
-                Zs.add(common_p , zs);
-                coeffs.push_back(coefficients[i]);
-            } else {
-                coeffs[common_l] += coefficients[i];
+            // If the z array is new, we add it to the Z_track for the Ps associated with num! 
+            if (!z_found){
+                Z_track[P_index].push_back(i);
             }
         }
     }
+    // Throw away the zero coefficients:
+    vector<cpp_int> Ps_kept;
+    ZVecs Z_track_kept;
+
+    for(int k = 0; k < Z_track.size(); k++){
+        vector<int> ztrack_k;
+        for(int l = 0; l < Z_track[k].size(); l++){
+            int k_l_index = Z_track[k][l];
+            complex<double> coeff_k_l = coeffs[k_l_index];
+            if (abs(coeff_k_l) > 1e-8){
+                //zero_coeffs_for_k.push_back(l);
+                ztrack_k.push_back(k_l_index);
+            }
+        }
+        if(ztrack_k.size() > 0){
+            Z_track_kept.push_back(ztrack_k);
+            Ps_kept.push_back(Ps[k]);
+        }
+    }
+
+    // Sorting everything based on indices of Ps:
+    vector<pair<cpp_int, int>> indexedP;
+    for(int i = 0; i < Ps_kept.size(); i++) {
+        indexedP.emplace_back(Ps_kept[i], i);
+    }
+
+    vector<cpp_int> Ps_sorted;
+    ZVecs Z_track_sorted;
+
+    sort(indexedP.begin() , indexedP.end());
+    for(int i = 0; i < Ps_kept.size(); i++){
+            int index = indexedP[i].second;
+            Ps_sorted.push_back(indexedP[i].first);
+            Z_track_sorted.push_back(Z_track_kept[index]);
+    }
+
+    PZ_data.coeffs = coeffs; //Coeffs and Zs are kept as the original data
+    PZ_data.Ps = Ps_sorted;
+    PZ_data.Zs = Zs;
+    PZ_data.Z_track = Z_track_sorted;
+
+    return PZ_data;
 }
 
+/*
 vector<int> index_keep(Coeffs coeffs){
     int s = coeffs.size();
     vector<int> indices(s);
@@ -150,75 +202,12 @@ using namespace boost;
 int main(){
     string fileName = "test.txt";  // Replace "input.txt" with the name of your input file
     vector<pair<complex<double>, vector<int>>> data = data_extract(fileName);
-
-    //vector<string> result = extractLinesFromFile(fileName);
-
-    // Printing the contents of the cell array
-    /*for (const auto& cell : result) {
-        cout << cell << endl;
-    }*/
-
-/*     vector<int> z1 = {1,2,3};
-    vector<int> z2 = {2,3};
-    vector<vector<int>> Z;
-    pair<complex<double>,vector<int>> data_1;
-    data_1.first = complex<double> (1.0, -1.5);
-    data_1.second = vector<int> {1,5,9,11};
-    vector<pair<complex<double>,vector<int>>> data;
-    data.push_back(data_1);
-    Z.push_back(z1);
-    Z.push_back(z2);
-    int l = Z.size();
-    for(size_t i = 0; i<l; i++){
-        cout << i << " th index of Z is ";
-        for(int element : Z[i]){
-            cout << element << " ";
-        }
-        cout << endl;
-    }
-    cout << "That was the end of Z" << endl;
-
-    cout << "first element of the data is " << data[0].first << endl;
-    cout << "second element of the data is ";
-    for(int element : data[0].second){
-            cout << element << " ";
-        }
-    cout << endl; */
-    
-    
-/*     string input = "1.240 1 4 5 2";
-    istringstream iss(input);
-
-    double realpart, complexpart=0;
-    char sign;
-    string complexPart;
-    //getline(iss, complexPart, ' ');
-    iss >> complexPart; 
-    istringstream complexIss(complexPart);
-    complexIss >> realpart >> complexpart;
-
-    //Extracting the vectors:
-
-    string token;
-    vector<int> integers;
-    while (iss >> token){
-        try {
-            int num = boost::lexical_cast<int>(token);
-            integers.push_back(num);
-        } catch (const boost::bad_lexical_cast&) {
-            // Ignore non-integer tokens
-        }
-    }
-    cout << "the real part is: " << realpart << endl; 
-
-    std::cout << "Integers: ";
-    for (const auto& num : integers) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl; */
-
+    PZdata PZ_data = PZcomp(data);
+    vector<cpp_int> Ps = PZ_data.Ps;
+    vector<complex<double>> coefficients = PZ_data.coeffs;
+    vector<vector<int>> Z_track = PZ_data.Z_track;
+    vector<vector<int>> Zs = PZ_data.Zs;
 
     return 0;
-
 }
 
